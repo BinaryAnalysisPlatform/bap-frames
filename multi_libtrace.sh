9cdd1c79a70c82cbfbf37bf646cea46f0451302f
Matthew Maurer <maurer@matthewmaurer.org>
2014-07-29 17:48:25 -0400

    Move multilib builds into _build
    Change-Id: I581b28ae08303db4b6e098422f9a05650600868d

--- multi_libtrace.sh
+++ multi_libtrace.sh
@@ -2,13 +2,13 @@
 cd libtrace
 ./autogen.sh
 cd ..
-mkdir libtrace-32
-pushd libtrace-32
-PREFIX=$PREFIX CPPFLAGS="$CPPFLAGS -m32" CXXFLAGS="$CXXFLAGS -m32" LDFLAGS="$LDFLAGS -m32" ../libtrace/configure
+mkdir -p _build/libtrace-32
+pushd _build/libtrace-32
+PREFIX=$PREFIX CPPFLAGS="$CPPFLAGS -m32" CXXFLAGS="$CXXFLAGS -m32" LDFLAGS="$LDFLAGS -m32" ../../libtrace/configure --libdir=$PREFIX/lib32
 make
 popd
-mkdir libtrace-64
-pushd libtrace-64
-PREFIX=$PREFIX ../libtrace/configure
+mkdir -p _build/libtrace-64
+pushd _build/libtrace-64
+PREFIX=$PREFIX ../../libtrace/configure --libdir=$PREFIX/lib64
 make
 popd
