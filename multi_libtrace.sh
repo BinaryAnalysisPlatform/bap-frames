89cf696de62afbe5af1c7889cf09c040dfa93c86
Dominick DiRenzo <ddirenzo@andrew.cmu.edu>
2014-08-01 14:32:07 -0400

    proper headers and multilib install
    Change-Id: I6ac3a711ebadd6cdebef9a0d3fd05081c4552acc

--- multi_libtrace.sh
+++ multi_libtrace.sh
@@ -4,11 +4,11 @@ cd libtrace
 cd ..
 mkdir libtrace-32
 pushd libtrace-32
-PREFIX=$PREFIX CPPFLAGS="$CPPFLAGS -m32" CXXFLAGS="$CXXFLAGS -m32" LDFLAGS="$LDFLAGS -m32" ../libtrace/configure
+PREFIX=$PREFIX CPPFLAGS="$CPPFLAGS -m32" CXXFLAGS="$CXXFLAGS -m32" LDFLAGS="$LDFLAGS -m32" ../libtrace/configure --libdir=$PREFIX/lib32
 make
 popd
 mkdir libtrace-64
 pushd libtrace-64
-PREFIX=$PREFIX ../libtrace/configure
+PREFIX=$PREFIX ../libtrace/configure --libdir=$PREFIX/lib64
 make
 popd
