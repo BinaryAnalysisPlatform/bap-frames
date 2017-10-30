fd2394185a763f2e281d11c721265563f4d107ba
Oleg <forown@yandex.ru>
2017-10-30 13:08:59 -0400

    renaming

--- lib/frame_enum.ml
+++ lib/frame_enum.ml
@@ -1,11 +1,11 @@
 open Core_kernel.Std
 
-module type E = sig
+module type Enumerated = sig
   type t [@@deriving enumerate]
 end
 
-module type Enum = sig
-  include E
+module type Enumerable = sig
+  include Enumerated
 
   val to_enum : t -> int
   val of_enum : int -> t option
@@ -13,20 +13,18 @@ module type Enum = sig
   val min : int
 end
 
-module type L = sig
-  include E
+module type Tabulated = sig
+  include Enumerated
   val list : (int * t) list
 end
 
-module Make_list(A : E) = struct
+module Tabulate(A : Enumerated) : Tabulated with type t = A.t = struct
   include A
   let list = List.mapi all ~f:(fun i x -> i, x)
 end
 
-module Make_enum(A : L) : Enum with type t := A.t = struct
+module Enumerate(A : Tabulated) : Enumerable with type t := A.t = struct
   include A
-
-  let sorted = List.sort ~cmp:(fun (i,_) (j,_) -> compare i j) list
 
   let to_enum t =
     fst @@ List.find_exn ~f:(fun (_,x) -> x = t) list
@@ -35,33 +33,40 @@ module Make_enum(A : L) : Enum with type t := A.t = struct
     List.find ~f:(fun (x,_) -> x = i) list |>
     Option.value_map ~f:(fun x -> Some (snd x)) ~default:None
 
-  let max = fst @@ List.last_exn sorted
-  let min = fst @@ List.hd_exn sorted
+  let max = match List.hd (List.rev list) with
+    | None -> 0
+    | Some x -> fst x
+
+  let min = match List.hd list with
+    | None -> 0
+    | Some x -> fst x
 end
 
-module type Substitute = sig
-  include E
+module type Substitution = sig
+  include Enumerated
   val subs : (t * int) list
 end
 
-module Make_substitution(S : Substitute) = struct
+module Substitute(S : Substitution) = struct
   include S
 
   let list =
-    List.map2_exn all
-      (List.init (List.length all) ~f:ident)
-      ~f:(fun t ind ->
-          match List.Assoc.find S.subs ~equal:(=) t with
-          | None -> ind, t
-          | Some ind -> ind, t)
+    List.rev @@
+    fst @@
+    List.fold all
+      ~init:([],0)
+      ~f:(fun (acc, ind) t ->
+          let subs_ind = List.Assoc.find S.subs ~equal:(=) t in
+          let ind = Option.value ~default:ind subs_ind in
+          (ind, t) :: acc, ind + 1)
 end
 
-module Make(A : E) = struct
-  module L = Make_list(A)
-  include Make_enum(L)
+module Make(A : Enumerated) = struct
+  module L = Tabulate(A)
+  include Enumerate(L)
 end
 
-module Make_substitute(S : Substitute) = struct
-  module L = Make_substitution(S)
-  include Make_enum(L)
+module Make_substitute(S : Substitution) = struct
+  module L = Substitute(S)
+  include Enumerate(L)
 end
--- test/test_enum.ml
+++ test/test_enum.ml
@@ -2,7 +2,7 @@ open Core_kernel.Std
 open OUnit2
 open Frame_enum
 
-type t = A | B | C [@@deriving enumerate]
+type t = A | B | C | D | E [@@deriving enumerate]
 
 module E = struct
   include Make(struct
@@ -13,7 +13,7 @@ end
 module S = struct
   include Make_substitute(struct
       type nonrec t = t [@@deriving enumerate]
-      let subs = [B, 42;]
+      let subs = [B, 42; D, 56;]
     end)
 end
 
@@ -24,19 +24,27 @@ let suite () =
     "of_enum 0" >:: check (E.of_enum 0 = Some A);
     "of_enum 1" >:: check (E.of_enum 1 = Some B);
     "of_enum 2" >:: check (E.of_enum 2 = Some C);
-    "of_enum 3" >:: check (E.of_enum 3 = None);
-    "max"       >:: check (E.max = 2);
+    "of_enum 3" >:: check (E.of_enum 3 = Some D);
+    "of_enum 4" >:: check (E.of_enum 4 = Some E);
+    "of_enum 5" >:: check (E.of_enum 5 = None);
+    "max"       >:: check (E.max = 4);
     "min"       >:: check (E.min = 0);
     "to_enum A" >:: check (E.to_enum A = 0);
     "to_enum B" >:: check (E.to_enum B = 1);
     "to_enum C" >:: check (E.to_enum C = 2);
+    "to_enum D" >:: check (E.to_enum D = 3);
+    "to_enum E" >:: check (E.to_enum E = 4);
     "substitute.of_enum 0" >:: check (S.of_enum 0 = Some A);
-    "substitute.of_enum 1" >:: check (S.of_enum 42 = Some B);
-    "substitute.of_enum 2" >:: check (S.of_enum 2 = Some C);
+    "substitute.of_enum 42" >:: check (S.of_enum 42 = Some B);
+    "substitute.of_enum 43" >:: check (S.of_enum 43 = Some C);
+    "substitute.of_enum 56" >:: check (S.of_enum 56 = Some D);
+    "substitute.of_enum 57" >:: check (S.of_enum 57 = Some E);
     "substitute.of_enum 3" >:: check (S.of_enum 1 = None);
-    "substitute.max"       >:: check (S.max = 42);
+    "substitute.max"       >:: check (S.max = 57);
     "substitute.min"       >:: check (S.min = 0);
     "substitute.to_enum A" >:: check (S.to_enum A = 0);
     "substitute.to_enum B" >:: check (S.to_enum B = 42);
-    "substitute.to_enum C" >:: check (S.to_enum C = 2);
+    "substitute.to_enum C" >:: check (S.to_enum C = 43);
+    "substitute.to_enum D" >:: check (S.to_enum D = 56);
+    "substitute.to_enum E" >:: check (S.to_enum E = 57);
   ]
